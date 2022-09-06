9c00f0a3e316336d3577b053e07c8d823428a54a
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-06 21:48:36 +0800

    add Exercise2.1

---
+++ Week2/Exercise2_1.m
@@ -0,0 +1,5 @@
+array1 = {0 0.5 2.1 -3.5 6.0; 0 -1.1 -6.6 2.8 3.4; 2.1 0.1 0.3 -0.4 1.3; 1.1 5.1 0.0 1.1 -2.0}
+array1_size = size(array1)
+value1 = array1(1,4)
+value2 = array1(:,1:2:5)
+size2 = array1([1 3],end)
