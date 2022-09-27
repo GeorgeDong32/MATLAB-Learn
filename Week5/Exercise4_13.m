6d63247614424dbdadafa1d364724da0ea90e629
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-27 17:44:10 +0800

    add week5

---
+++ Week5/Exercise4_13.m
@@ -0,0 +1,15 @@
+% Script file: Exercise4_13.m
+%
+% Purpose: 
+% Dealing with Refraction Issues
+%
+% Record of revisions:
+% Date     Programmer   Description of change
+% ======== ============ =====================
+% 22/09/27 GeorgeDong32 Original code 
+%
+% Define variables: 
+% theta_in Incidence angle
+% theta_out Angle of refraction
+% n_1 Index of Refraction 1
+% n_2 Index of Refraction 2
