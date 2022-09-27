181f9024c71306ce315c2fde857738b2c6db4192
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-27 18:00:54 +0800

    Update Exercise4_13.m

--- Week5/Exercise4_13.m
+++ Week5/Exercise4_13.m
@@ -13,3 +13,11 @@
 % theta_out Angle of refraction
 % n_1 Index of Refraction 1
 % n_2 Index of Refraction 2
+
+% Input and data processing
+theta_in = input('Please input the incidence angle(degree)');
+n_1 = input('Please input n1');
+n_2 = input('Please input n2');
+theta_out = asind((n_1/n_2) * sin(theta_in/(2 * pi)));
+
+% Draw
