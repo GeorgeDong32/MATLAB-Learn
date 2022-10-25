7aec2f55b8f4147561b8d645178e93351611d2e1
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-25 16:27:54 +0800

    Init

---
+++ Week8/Exercise8_20.m
@@ -0,0 +1,11 @@
+% Script file: Exercise8_20.m
+%
+% Purpose:
+% Dealing with Complex number plots
+%
+% Record of revisions:
+% Date     Programmer   Description of change
+% ======== ============ =====================
+% 22/10/18 GeorgeDong32 Version 1.1
+%
+% Define variables:
