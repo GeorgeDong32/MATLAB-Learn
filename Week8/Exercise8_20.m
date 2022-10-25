56ec6b91939b7c0d2d55e14511cf9927714ebcf9
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-25 20:06:47 +0800

    Update

--- Week8/Exercise8_20.m
+++ Week8/Exercise8_20.m
@@ -6,6 +6,26 @@
 % Record of revisions:
 % Date     Programmer   Description of change
 % ======== ============ =====================
-% 22/10/18 GeorgeDong32 Version 1.1
+% 22/10/18 GeorgeDong32 Version 1.0
 %
-% Define variables:
+% Defined variables:
+% [x y] plots' x and y values
+% z plots' z values
+
+[x, y] = meshgrid(-1:0.01:1, (-2 * pi):(0.02 * pi):(2 * pi));
+z = exp(x + y * 1i);
+
+%drawing mesh plot
+figure();
+mesh(x, y, real(z));
+title('Mesh Plot of 8.20');
+
+%draw surface plot
+figure();
+surf(x, y, real(z));
+title('Surface Plot of 8.20');
+
+%draw contour plot
+figure();
+contourf(x, y, real(z));
+title('fContour Plot of 8.20');
