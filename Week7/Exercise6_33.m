a7ac7bb0aab189ab5a4bb993f6f894e4c668f464
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-25 16:27:43 +0800

    Init

--- Week7/Exercise6_33.m
+++ Week7/Exercise6_33.m
@@ -14,17 +14,16 @@
 % var_mynd deviation of myRandom function
 
 % Generate random numbers
-set(gcf,'unit','normalized','position',[0.2,0.2,0.55,0.55]);
-stdnd = randn(100000,1);
-histogram(stdnd,50,'Normalization','probability');   
+set(gcf, 'unit', 'normalized', 'position', [0.2, 0.2, 0.55, 0.55]);
+stdnd = randn(100000, 1);
+histogram(stdnd, 50, 'Normalization', 'probability');
 hold on;
-mynd=myRandom(100000);
-histogram(mynd,50,'Normalization','probability','EdgeAlpha',0.3);
+mynd = myRandom(100000);
+histogram(mynd, 50, 'Normalization', 'probability', 'EdgeAlpha', 0.3);
 % Calculate variance
 var_mynd = var(mynd);
 % Add annotations
-legend("Standard normal distribution","My normal distribution")
+legend("Standard normal distribution", "My normal distribution")
 title("通过随机分布生成正态分布");
-text(-4,0.05,['生成函数方差=', num2str(var_mynd)]);
+text(-4, 0.05, ['生成函数方差=', num2str(var_mynd)]);
 hold off;
- 
