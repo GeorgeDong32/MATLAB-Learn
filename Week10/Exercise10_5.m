aabc9e315fc2b388f8a5938c4eee584869703073
George·Dong <georgedong32@foxmail.com>
2022-11-08 14:59:18 +0800

    Init

---
+++ Week10/Exercise10_5.m
@@ -0,0 +1,10 @@
+% Script file: Exercise10_5.m
+%
+% Purpose:
+%
+% Record of revisions:
+% Date     Programmer   Description of change
+% ======== ============ =====================
+% 22/11/8 GeorgeDong32 Version 1.0
+%
+% Defined variables:
---
+++ Week10/Exercise9_8.m
@@ -0,0 +1,11 @@
+% Script file: Exercise9_8.m
+%
+% Purpose:
+% insert "not" after every "is"
+%
+% Record of revisions:
+% Date     Programmer   Description of change
+% ======== ============ =====================
+% 22/11/8 GeorgeDong32 Version 1.0
+%
+% Defined variables:
