62ec0f31ca89a6c0cb6163428b04ca5c30dfa0ba
George·Dong <georgedong32@foxmail.com>
2022-11-08 20:38:39 +0800

    Update

--- Week10/Exercise10_5.m
+++ Week10/Exercise10_5.m
@@ -1,6 +1,7 @@
 % Script file: Exercise10_5.m
 %
 % Purpose:
+% Using structure array to plot a data set
 %
 % Record of revisions:
 % Date     Programmer   Description of change
@@ -13,4 +14,23 @@
 x3 = -2 * pi:0.01:2 * pi;
 y3 = sin(x3);
 
+% 有所有值的structure
 plot_info(3) = struct('x_data', x3, 'y_data', y3, 'type', 'linear', 'plot_title', 'sin(x)', 'x_label', 'x', 'y_label', 'y', 'x_range', [-2 * pi, 2 * pi], 'y_range', [-1, 1]);
+MyPlotFunction(plot_info(3))
+
+% 没有type和title的structure
+x2 = -2 * pi:0.01:2 * pi;
+plot_info(2).x_data = -2 * pi:0.01:2 * pi; plot_info(2).y_data = cos(x2);
+plot_info(2).x_label = 'x'; plot_info(2).y_label = 'y';
+plot_info(2).x_range = [-2 * pi, 2 * pi]; plot_info(2).y_range = [-1, 1];
+MyPlotFunction(plot_info(2))
+
+% 没有label和range的structure
+plot_info(3).x_data = -2 * pi:0.01:2 * pi; plot_info(3).y_data = tan(x2);
+plot_info(3).type = 'linear'; plot_info(3).plot_title = 'tan(x)';
+MyPlotFunction(plot_info(3))
+
+% 没有data的structure,应报错
+plot_info(4).type = 'linear';
+plot_info(4).plot_title = 'No data';
+MyPlotFunction(plot_info(4))
