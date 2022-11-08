02352a94f8aeca25d23fee1369003a83cf604f9e
George·Dong <georgedong32@foxmail.com>
2022-11-08 16:28:04 +0800

    Update

--- Week10/Exercise10_5.m
+++ Week10/Exercise10_5.m
@@ -8,3 +8,9 @@
 % 22/11/8 GeorgeDong32 Version 1.0
 %
 % Defined variables:
+% plot_info plot信息Structure数组
+
+x3 = -2 * pi:0.01:2 * pi;
+y3 = sin(x3);
+
+plot_info(3) = struct('x_data', x3, 'y_data', y3, 'type', 'linear', 'plot_title', 'sin(x)', 'x_label', 'x', 'y_label', 'y', 'x_range', [-2 * pi, 2 * pi], 'y_range', [-1, 1]);
--- Week10/Exercise9_8.m
+++ Week10/Exercise9_8.m
@@ -9,3 +9,18 @@
 % 22/11/8 GeorgeDong32 Version 1.0
 %
 % Defined variables:
+% inputstr input string 输入的字符串
+% targetstr 目标字符串
+% replacestr 替换字符串
+% resultstr 结果字符串
+
+% test code
+inputstr = 'This sentence is a test, is it not';
+
+% User input
+% inputstr = input('Please input your string')
+
+targetstr = 'is';
+replacestr = 'is not';
+resultstr = strrep(inputstr, targetstr, replacestr);
+fprintf('The result is: \"%s\"', resultstr)
