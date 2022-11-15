6abd7bf68513c189ff346f8b85121b694a62a845
George·Dong <georgedong32@foxmail.com>
2022-11-15 21:50:12 +0800

    sdv week10

--- Week11/CalculationGA.m
+++ Week11/CalculationGA.m
@@ -1,30 +1,22 @@
 function GA = CalculationGA(height)
-%CALCULATIONGA
-% 
-% Purpose:
-% Calculate Gravitational Acceleration
-%
-% Record of revisions:
-% Date     Programmer   Description of change
-% ======== ============ =====================
-% 22/11/15 GeorgeDong32 Version 1.0
-%
-% Defined variables:
-% G 万有引力常量
-% M 地球质量
-% R 地球半径
-% GA 重力加速度计算值
+    %CALCULATIONGA
+    %
+    % Purpose:
+    % Calculate Gravitational Acceleration
+    %
+    % Record of revisions:
+    % Date     Programmer   Description of change
+    % ======== ============ =====================
+    % 22/11/15 GeorgeDong32 Version 1.0
 
-loopctrl = length(height); % 循环控制
-mid = zeros(1,loopctrl);
-G = 6.672 * 10^(-11);
-M = 5.98 * 10^24;
-R = 6371 * 10^3;
+    gdctrl = length(height);
+    GA = zeros(1, gdctrl);
+    G = 6.672 * 10^(-11);
+    M = 5.98 * 10^24;
+    R = 6371 * 10^3;
 
-for i = 1:loopctrl
-   mid(i) = (-G)*M/(R+height(i)*10^3)^2;
-end
-GA = mid;
+    for i = 1:gdctrl
+        GA(i) = (-G) * M / (R + height(i) * 10^3)^2;
+    end
 
 end
-
--- Week11/Exercise11_4.m
+++ Week11/Exercise11_4.m
@@ -7,43 +7,18 @@
 % Date     Programmer   Description of change
 % ======== ============ =====================
 % 22/11/15 GeorgeDong32 Version 1.0
-%
-% Defined variables:
-% height 高度数据
-% GA 重力加速度数据 
-% GA 重力加速度数据集
 
-% calculation
 height = 0:500:40000;
 GA = CalculationGA(height);
-GAData(82) = struct('Height',0,'g',-9.82973956335312,'unit','m/s^2');
-for i=1:81
+GAData(82) = struct('Height', 0, 'g', -9.82973956335312, 'unit', 'm/s^2');
+
+for i = 1:81
     GAData(i).Height = height(i);
     GAData(i).g = GA(i);
     GAData(i).unit = 'm/s^2';
 end
 
-% plot
 figure()
-plot(height,GA);
-ylabel('\bf重力加速度值/ m/s^2')
-xlabel('\bf高度/ km')
-title('\bf重力加速度图像')
+plot(height, GA);
 
-% Export to file
-save GADataFile GAData; % To .mat
-
-% 使用文件数据绘制图像
-clear
-load GADataFile.mat
-figure();
-height2 = zeros(81,1);
-g2 = zeros(81,1);
-for i=1:81
-    height2(i) = GAData(i).Height;
-    g2(i) = GAData(i).g;
-end
-plot(height2,g2)
-ylabel('\bf重力加速度值/ m/s^2')
-xlabel('\bf高度/ km')
-title('\bf重力加速度图像2')
+save GADataFile GAData;
