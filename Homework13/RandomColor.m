f2531dba786b6d9f3a898d333c7c55961dd75a13
George·Dong <georgedong32@foxmail.com>
2022-12-06 17:24:15 +0800

    Init

---
+++ Homework13/RandomColor.m
@@ -0,0 +1,35 @@
+function colorStr = RandomColor()
+    % Script file: RandomColor.m
+    %
+    % Purpose:
+    % generate a random color string for plot
+    %
+    % Record of revisions:
+    % Date     Programmer   Description of change
+    % ======== ============ =====================
+    % 22/12/6  GeorgeDong32 Version 1.0
+    %
+    % Defined variables:
+    % colorStr   the random color string
+    % stdc       1/7
+    % x          a random number
+    stdc = 1/7;
+    x = rand();
+
+    if (x >= 0 && x < stdc)
+        colorStr = 'red';
+    elseif (x >= stdc && x < 2 * stdc)
+        colorStr = 'green';
+    elseif (x >= 2 * stdc && x < 3 * stdc)
+        colorStr = 'blue';
+    elseif (x >= 3 * stdc && x < 4 * stdc)
+        colorStr = 'yellow';
+    elseif (x >= 4 * stdc && x < 5 * stdc)
+        colorStr = 'cyan';
+    elseif (x >= 5 * stdc && x < 6 * stdc)
+        colorStr = 'magenta';
+    else
+        colorStr = 'black';
+    end
+
+end
