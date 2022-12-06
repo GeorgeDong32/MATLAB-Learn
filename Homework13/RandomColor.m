b16f080c2b259040d70197fb4334c5c8a90fa2b6
George·Dong <georgedong32@foxmail.com>
2022-12-06 19:45:39 +0800

    v2.0

--- Homework13/RandomColor.m
+++ Homework13/RandomColor.m
@@ -7,20 +7,31 @@ function colorStr = RandomColor()
     % Record of revisions:
     % Date     Programmer   Description of change
     % ======== ============ =====================
-    % 22/12/6  GeorgeDong32 Version 1.0
-    gd = 1/7;
+    % 22/12/6  GeorgeDong32 Version 2.0
+    %
+    % Defined variables:
+    % colorStr   the random color string
+    % stdc       1/7
+    % x          a random number
+    stdc = 1/7;
     x = rand();
 
-    if (x >= 0 && x < gd)
+    if (x >= 0 && x < stdc)
         colorStr = 'red';
-    elseif (x >= gd && x < 2 * gd)
+    elseif (x >= stdc && x < 2 * stdc)
         colorStr = 'green';
-    elseif (x >= 2 * gd && x < 3 * gd)
+    elseif (x >= 2 * stdc && x < 3 * stdc)
         colorStr = 'blue';
-    elseif (x >= 3 * gd && x < 4 * gd)
+    elseif (x >= 3 * stdc && x < 4 * stdc)
         colorStr = 'yellow';
+    elseif (x >= 4 * stdc && x < 5 * stdc)
+        colorStr = 'cyan';
+    elseif (x >= 5 * stdc && x < 6 * stdc)
+        colorStr = 'magenta';
     else
         colorStr = 'black';
     end
 
+    % test codes
+    %disp(colorStr);
 end
