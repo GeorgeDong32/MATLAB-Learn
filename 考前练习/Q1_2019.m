c61b0206c99d9943872cb473b19d7d93a8e2f3a1
George·Dong <georgedong32@foxmail.com>
2023-01-07 16:57:12 +0800

    Init

---
+++ 考前练习/Q1_2019.m
@@ -0,0 +1,24 @@
+% Script file: Q1_2019.m
+%
+% Purpose:
+% Calculate some numbers
+%
+% Record of revisions:
+% Date     Programmer   Description of change
+% ======== ============ =====================
+% 23/1/7   GeorgeDong32 Version 1.0
+%
+% Defined variables:
+% x,y
+
+x = 2.2;
+y = 4;
+
+ans1 = (4/3) * pi * y * y;
+ans2 = (2 * y ^ -2) / (x + y) ^ 2;
+ans3 = (y ^ 3) / (y ^ 3 - x ^ 3);
+ans4 = ans1;
+disp(ans1)
+disp(ans2)
+disp(ans3)
+disp(ans4)
