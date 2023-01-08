0cda51c532edefdd9f02103bae89063d4defaccb
George·Dong <georgedong32@foxmail.com>
2023-01-08 22:28:49 +0800

    0

---
+++ 考前练习/Q4_mtxdisp.m
@@ -0,0 +1,20 @@
+function Q4_mtxdisp(cmatrix)
+    % Script file: Q4_mtxdisp.m
+    %
+    % Purpose:
+    % This function displays a matrix in a formatted way.
+    %
+    % Record of revisions:
+    % Date     Programmer   Description of change
+    % ======== ============ =====================
+    % 23/1/8  GeorgeDong32 Version 1.0
+    %
+    % Defined variables:
+    % cmatrix -- the matrix to be displayed
+    l = length(cmatrix);
+    for i = 1:1:2
+        for j = 1:1:l
+            if(abs(cmatrix(i,j)) < 1)
+        end
+    end
+end
