8d681b6cd0664386394b984036ff298497fc336f
GeorgeDong32 <georgedong32@foxmail.com>
2023-01-08 23:27:53 +0800

    update

--- 考前练习/Q4_mtxdisp.m
+++ 考前练习/Q4_mtxdisp.m
@@ -12,9 +12,25 @@ function Q4_mtxdisp(cmatrix)
     % Defined variables:
     % cmatrix -- the matrix to be displayed
     l = length(cmatrix);
+
     for i = 1:1:2
+
         for j = 1:1:l
-            if(abs(cmatrix(i,j)) < 1)
+
+            if (abs(cmatrix(i, j)) < 1)
+                fprintf('            ')
+            elseif (imag(cmatrix(i, j)) == 0)
+                fprintf('    %.2f    ', real(cmatrix(i, j)))
+            else
+                fprintf('%.2fexp(%.2fi)', sqrt(real(cmatrix(i, j) ^ 2 + imag(cmatrix(i, j) ^ 2))), atan(imag(cmatrix(i, j)) / real(cmatrix(i, j))))
+            end
+
+            if (j == l)
+                fprintf('\n');
+            end
+
         end
+
     end
+
 end
